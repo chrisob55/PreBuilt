04f6c32bd8427e33f8e5a9371eda5439ac4d0332
Christopher O'Brien <chris.obrien@digital.hmrc.gov.uk>
2019-10-18 11:13:19 +0100

    master - changes

--- PreBuilt/Foo.swift
+++ PreBuilt/Foo.swift
@@ -1,14 +1,10 @@
-//
-//  Foo.swift
-//  PreBuilt
-//
-//  Created by Christopher O'Brien on 18/10/2019.
-//  Copyright © 2019 Christopher O'Brien. All rights reserved.
-//
 
 import Foundation
 
-public class Foo {
+public struct Foo {
+    public init() {
+        
+    }
     public func bar() -> String {
         return "Bar executed"
     }
