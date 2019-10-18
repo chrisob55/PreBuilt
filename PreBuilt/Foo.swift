08b1e03fae0b74271a7f85ebf007781afd17e3d3
Christopher O'Brien <chris.obrien@digital.hmrc.gov.uk>
2019-10-18 10:09:58 +0100

    master - Setup

---
+++ PreBuilt/Foo.swift
@@ -0,0 +1,15 @@
+//
+//  Foo.swift
+//  PreBuilt
+//
+//  Created by Christopher O'Brien on 18/10/2019.
+//  Copyright © 2019 Christopher O'Brien. All rights reserved.
+//
+
+import Foundation
+
+open class Foo {
+    open func bar() -> String {
+        return "Bar executed"
+    }
+}
