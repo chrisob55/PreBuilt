f4eb8a066faa555206baad2f5d799182336edae7
Christopher O'Brien <chris.obrien@digital.hmrc.gov.uk>
2019-10-18 13:06:10 +0100

    master - binary

---
+++ Carthage 2/Build/iOS/PreBuilt.framework/Headers/PreBuilt.h
@@ -0,0 +1,19 @@
+//
+//  PreBuilt.h
+//  PreBuilt
+//
+//  Created by Christopher O'Brien on 18/10/2019.
+//  Copyright © 2019 Christopher O'Brien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreBuilt.
+FOUNDATION_EXPORT double PreBuiltVersionNumber;
+
+//! Project version string for PreBuilt.
+FOUNDATION_EXPORT const unsigned char PreBuiltVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreBuilt/PublicHeader.h>
+
+
--- PreBuilt/Foo.swift
+++ PreBuilt/Foo.swift
@@ -17,4 +17,7 @@ public struct Foo {
     public func ghr() -> String {
         return "GHR executed"
     }
+    public func binary() -> String {
+        return "Binary executed"
+    }
 }
